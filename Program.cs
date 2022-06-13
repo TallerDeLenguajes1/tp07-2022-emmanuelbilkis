e74e6d594e5d57c950a3c878b13c478e2e6f8f78
emmanuelbilkis <emmanuelbilkis@gmail.com>
2022-06-13 10:43:02 -0300

    finalizacion de los apartados, falta convertilos a funciones

--- Program.cs
+++ Program.cs
@@ -8,25 +8,73 @@ namespace toDo
     {
         static void Main(string[] args)
         {
-            List<empleados> ListaEmpleados = new List<empleados>();
+            List<tareas> ListaTareas = new List<tareas>();
 
-            string salida = '';
+            Random N = new Random();
+            int aleat = N.Next(1,10);
             int num = 0;
-            int duracion=0;
+            Console.WriteLine("Aleatorio: " + aleat);
 
-            do
+            for (int i = 0; i < aleat; i++)
             {
-                Console.WriteLine("Ingrese la descripcion del empleado");
-                string descripcionEmpleado = Console.Readline();
 
-                empleados Nempleado = new empleados();
-                Nempleado.descripcion = descripcionEmpleado;
+                string descripcionEmpleado = Console.ReadLine();
+
+                tareas Ntarea = new tareas();
+                Ntarea.Descripcion = descripcionEmpleado;
+
                 num++;
-                Nempleado.ID = num;
+                Ntarea.ID1 = num;
 
                 Console.WriteLine("Ingrese la duracion");
+                Ntarea.Duracion =Convert.ToInt32(Console.ReadLine());
+
+                Ntarea.Estado = estadoTarea.pendiente;
+
+                ListaTareas.Add(Ntarea);
+
+                /*Console.WriteLine("Desea agregar otro empleado ? s/n");
+                salida = Console.ReadLine();*/
+            
+            }
+        
+            foreach (tareas xtarea in ListaTareas)
+            {
+                Console.WriteLine("ID: " + xtarea.ID1 + "| Descripcion: " + xtarea.Descripcion + "| Duracion: "+ xtarea.Duracion);
+                Console.WriteLine("Estado: " + xtarea.Estado);
+            }
+
+            for (int i = 0; i < ListaTareas.Count; i++)
+            {
+                Console.WriteLine("Desea cambiar el estado de la tarea ? s/n");
+                string entrada = Console.ReadLine();
+
+                if (entrada == "s")
+                {
+                    ListaTareas[i].Estado = estadoTarea.realizada;
+                }
                 
-            } while (salida != "n");
+            }
+
+
+            Console.WriteLine("Ingrese la descripcion de la tarea a buscar");
+            string descripcion = Console.ReadLine();
+            foreach (tareas xtarea in ListaTareas)
+            {
+                if (xtarea.Descripcion == descripcion && xtarea.Estado == estadoTarea.realizada)
+                {
+                     Console.WriteLine("ID: " + xtarea.ID1 + "| Descripcion: " + xtarea.Descripcion + "| Duracion: "+ xtarea.Duracion);
+                     Console.WriteLine("Estado: " + xtarea.Estado);
+                }
+            }
+            
+            int sumador=0;
+            foreach (tareas xtarea in ListaTareas)
+            {
+                sumador = sumador + xtarea.Duracion;
+            }
+
+            Console.WriteLine("La sumatoria de las horas trabajadas por los emppleados son : " + sumador);
 
         }
     }
--- empleados.cs
+++ empleados.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 
 namespace toDo
 {
-    class empleados
+    public enum estadoTarea
     {
-        int ID;
-        string descripcion;
-        int duracion; // entre 10 y 100
+        pendiente = 0,
+        haciendo = 1,
+        realizada = 2 
+        
+    }
+    public class tareas
+    {
+        private int ID;
+        private string descripcion;
+        private int duracion; // entre 10 y 100
+        private estadoTarea estado;
+
+        public int ID1 { get => ID; set => ID = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public int Duracion { get => duracion; set => duracion = value; }
+        public estadoTarea Estado { get => estado; set => estado = value; }
     }    
 }
