5bd20a7b1957134a0407fdc38a4d73b7d7b98b57
emmanuelbilkis <emmanuelbilkis@gmail.com>
2022-06-13 09:32:00 -0300

    creacion de la clase empleados y puesta en marcha la interfaz para cargar

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace toDo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<empleados> ListaEmpleados = new List<empleados>();
+
+            string salida = '';
+            int num = 0;
+            int duracion=0;
+
+            do
+            {
+                Console.WriteLine("Ingrese la descripcion del empleado");
+                string descripcionEmpleado = Console.Readline();
+
+                empleados Nempleado = new empleados();
+                Nempleado.descripcion = descripcionEmpleado;
+                num++;
+                Nempleado.ID = num;
+
+                Console.WriteLine("Ingrese la duracion");
+                
+            } while (salida != "n");
+
+        }
+    }
+}
---
+++ empleados.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace toDo
+{
+    class empleados
+    {
+        int ID;
+        string descripcion;
+        int duracion; // entre 10 y 100
+    }    
+}
